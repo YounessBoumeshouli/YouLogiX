23acac2430a427ac4a461f42841ed762f5a9796c
youness boumeshouli <boumeshouliyouness6@gmail.com>
2026-01-06 09:17:42 +0100

    running tests in main.py

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from fastapi import FastAPI
 from routes.client_routes import router as client_router
 
 app = FastAPI()
+@app.on_event("startup")
+def run_tests_on_startup():
+    print("🚀 Vérification de la connexion base de données...")
+    exit_code = pytest.main(["tests/test_connection.py"])
+    if exit_code != 0:
+        print("❌ Les tests ont échoué. Vérifiez votre configuration .env")
+    else:
+        print("✅ Tests réussis !")
 
+app.include_router(client_router)
 app.include_router(client_router)
 
 @app.get("/")
