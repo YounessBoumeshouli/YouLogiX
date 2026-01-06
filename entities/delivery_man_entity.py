2f937074816509db961ae81c7f1cf8faad01718f
youness boumeshouli <boumeshouliyouness6@gmail.com>
2026-01-06 14:23:47 +0100

    fix historical status

--- entities/delivery_man_entity.py
+++ entities/delivery_man_entity.py
@@ -1,8 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from app.db.database import Base
 from enums.vehicule_enum import EnumVehicule
 
-class Delivery_man(Base):
+class Delivery_man(User):
     __tablename__ = "delivery_men"
 
     id = Column(Integer, ForeignKey("users.id"), primary_key=True)
