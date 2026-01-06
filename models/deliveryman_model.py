6c91ca15869205d30356ff22be019658ec1e920e
youness boumeshouli <boumeshouliyouness6@gmail.com>
2026-01-06 12:13:29 +0100

    create deliveryman model

--- models/deliveryman_model.py
+++ models/deliveryman_model.py
@@ -1,10 +1,22 @@
 import User
 import Parcel
-import etities.deliveryman_entity
+from etities.deliveryman_entity import Delivery_man
 from entities.enums.Parcel_enum import Parcel_enum
-
+from app.schemas.delivery_man import DeliveryManCreate
 class Deliveryman(User):
 
-    def assignParcel(self , parcel_id)->bool:
+    def assignParcel(self ,db, parcel_id)->bool:
         parcel =  Parcel.getParcel(parcel_id)
-        return 0 if parcel.status == Parcel_enum.LIVRED else 1
+        return False if parcel.status == Parcel_enum.LIVRED else True
+    def deliveryMan_create(self , delivery_man_in: DeliveryManCreate)->bool:
+        delivery_man_data = delivery_man_in.model_dump()
+
+        db_delivery_man = Client(
+            **delivery_man_data,
+            hashed_password=hashed_password
+        )
+
+        db.add(db_delivery_man)
+        db.commit()
+        db.refresh(db_delivery_man)
+        return db_delivery_man
