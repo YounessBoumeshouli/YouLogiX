5abaf44186bc9248bc1597fc6ccd76f3c5b668f0
anass17 <anassboutaib2018@gmail.com>
2026-01-09 12:10:14 +0100

    Fix - db connection & client tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -4,6 +4,9 @@ from tests.conftest import engine  # Import the test engine
 
 
 def test_database_connection():
+    
+    Base.metadata.create_all(engine)
+
     # Use the inspector to see if tables were created in the test sqlite
     inspector = inspect(engine)
     tables = inspector.get_table_names()
